d5c6edfeb90dba7d84a0e9f34b2e0f218470ddf9
cuidemin1987 <cuidemin1987@naver.com>
2025-05-23 21:29:43 +0900

    home 20250523-1

--- src/basic/PrintMatrix.java
+++ src/basic/PrintMatrix.java
@@ -4,16 +4,44 @@ public class PrintMatrix {
     public static void main(String[] args) {
         // printmatrix 문
 
-        for(int i=0; i < 101; i++ ) {
-            int ten = i/10;
+//        for(int i=0; i < 101; i++ ) {
+//            int ten = i/10;
+//            int il = i % 10;
+//
+//            if(i%3==0)
+//                System.out.print("짝\t");
+//            else
+//                System.out.printf("%d\t", i);
+//
+//            if(i%10==0) { // 10 단위 체크
+//                System.out.println();
+//            }
+//        }
+//
+//    }
+//============================================================
+
+        for(int i = 0; i <= 100; i++ ) {
+            int ten = i / 10;
             int il = i % 10;
 
-            if(i%3==0)
+
+            if(ten == 3 || ten == 6 || ten == 9) {
+                ten++;
                 System.out.print("짝\t");
-            else
+            }
+
+            if(il == 3 || il == 6 || il == 9) {
+                il++;
+                System.out.print("짝\t");
+            }
+
+            if(i % 3==0) {
+                System.out.print("짝\t");
+            }else
                 System.out.printf("%d\t", i);
 
-            if(i%10==0) { // 10 단위 체크
+            if(i % 10 == 0) { // 10 단위 체크
                 System.out.println();
             }
         }
